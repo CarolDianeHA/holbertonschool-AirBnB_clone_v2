7f3c2f2cba6392e7eee13e7884b5736dadbebd83
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-06-12 19:44:06 -0400

    Fixing PEP8.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,14 +8,17 @@ from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/states_list')
 def states_list():
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def close(self):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
