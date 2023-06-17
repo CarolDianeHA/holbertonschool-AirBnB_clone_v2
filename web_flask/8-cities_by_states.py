51b0dd76cb45f221a8e3fb9f691f171a377c8576
CarolDianeHA <carol.d.hernandez@outlook.com>
2023-06-17 09:54:23 -0400

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,17 +9,21 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
+@app.route('/state_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
-    return render_template('cities_by_states.html', states=sorted_states)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
 
 
 if __name__ == '__main__':
